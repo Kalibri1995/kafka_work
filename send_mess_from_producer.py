0cc80a3cb3eecce08fb88e3a0c9de6ca9a41cee6
Kalibri1995 <gainatullinalba@gmail.com>
2025-03-01 13:39:34 +0300

    Получилось !!!!

--- send_mess_from_producer.py
+++ send_mess_from_producer.py
@@ -1,6 +1,7 @@
 from kafka import KafkaProducer
 import json
 
+
 class Alert:
     def __init__(self, alert_id, stage, level, message):
         self.alert_id = alert_id
@@ -19,17 +20,20 @@ class Alert:
             "message": self.message
         }
 
+
 def on_send_success(record_metadata):
     print(f"Message sent to {record_metadata.topic} "
           f"partition {record_metadata.partition} "
           f"offset {record_metadata.offset}")
 
+
 def on_send_error(excp):
     print(f"Message failed to send: {excp}")
 
+
 # Создаем продюсера Kafka
 producer = KafkaProducer(
-    bootstrap_servers=["kafka1:9092", "kafka2:9093", "kafka3:9094"],  # Укажи свой адрес брокера Kafka
+    bootstrap_servers=["localhost:29092", "localhost:29093", "localhost:29094"],
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
